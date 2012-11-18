408439bd01fdad2af0d52a4c22b7bf5a0a6b11d9
Mike Bayer <mike_mp@zzzcomputing.com>
2012-11-18 12:52:29 -0500

    move description into version

--- site/js/doc_versions.js
+++ site/js/doc_versions.js
@@ -5,19 +5,19 @@ var doc_versions = [
         "slug":"latest"
       },
       {
-        "version":"0.8",
+        "version":"Version 0.8",
         "slug":"rel_0_8"
       },
       {
-        "version":"0.7",
+        "version":"Version 0.7",
         "slug":"rel_0_7"
       },
       {
-        "version":"0.6",
+        "version":"Version 0.6",
         "slug":"rel_0_6"
       },
       {
-        "version":"0.5",
+        "version":"Version 0.5",
         "slug":"rel_0_5"
       }
     ];
@@ -28,7 +28,7 @@ function renderDocVersions() {
     for (key in doc_versions) {
         obj = doc_versions[key];
         current_url = docs_base + "/en/" + obj.slug + "/";
-        $("#version_menu,.version-listing").append('<li><a href="' + current_url + '">Version ' + obj.version + '</a></li>');
+        $("#version_menu,.version-listing").append('<li><a href="' + current_url + '">' + obj.version + '</a></li>');
     }
 }
 
