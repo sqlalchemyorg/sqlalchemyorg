9fb31a3257214fca8a34a90beec2f5eaf21c06ee
Mike Bayer <mike_mp@zzzcomputing.com>
2021-11-07 12:56:17 -0500

    move to libsass

--- _controllers/gen_scss.py
+++ _controllers/gen_scss.py
@@ -1,20 +1,19 @@
 import os
 from zeekofile.cache import zf
-from scss import Scss
+import sass
 
 # these docs aren't super accurate
 # http://pyscss.readthedocs.org/en/latest/
 
 def run():
     # Available values are 'nested', 'expanded', 'compact', 'compressed' and 'legacy':
-    compiler = Scss(scss_opts={"style": "expanded"})
     for fname in os.listdir("_scss"):
         name, ext = os.path.splitext(fname)
         if ext != ".scss":
             continue
 
         with open(os.path.join("_scss", fname)) as _file:
-            css = compiler.compile(_file.read())
+            css = sass.compile(string=_file.read())
         with open(
                 zf.util.path_join(zf.writer.output_dir, "css", "%s.css" % name),
                 "w") as out:
